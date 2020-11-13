d402718dc8739664416f869f8ac096361569075e
willmondal <wbmondal@aol.com>
2020-11-13 09:41:23 -0800

    complete

--- index.js
+++ index.js
@@ -56,16 +56,11 @@ const zooAnimals = [
   The zoos want to display both the scientific name and the animal name in front of the habitats. Populate the displayNames array with only the animal_name and scientific_name of each animal. displayNames will be an array of strings, and each string should follow this pattern: "Name: Jackal, asiatic, Scientific: Canis aureus."
   */
   function animalNames(data){
-    const displayNames =[];
-    let animalName = [];
-    let scientificNames = [];
+    const displayNames = [];
     data.forEach(function(item){
-      animalName.push(item.animal_name)
+      return displayNames.push(`name: ${item.animal_name}, scientific: ${item.scientific_name}`)
     })
-    data.forEach(function(item){
-      scientificNames.push(item.scientific_name)
-    })
-    
+    return displayNames
   }
   
   
@@ -127,8 +122,8 @@ const zooAnimals = [
   }
 
  // 🦁🦁🦁 Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!" 🦁🦁🦁
-  function greeting(/*Your Code Here */){
-   return /*Your Code Here */
+  function greeting(firstName, lastName){
+   return `Hello ${firstName} ${lastName}, nice to meet you!`
   }
   
   // 🦁🦁🦁 Step 3: Check your work by un-commenting the following calls to consume(): 🦁🦁🦁 
@@ -149,8 +144,10 @@ const zooAnimals = [
 /* 🐴🐴🐴 Step 1: Base Constructor 🐴🐴🐴
   Create a constructor function named CuboidMaker that accepts properties for length, width, and height which can be initialized as an object
 */
-function CuboidMaker(/*Your Code Here */){
-  /*Your Code Here */
+function CuboidMaker(attributes){
+  this.length = attributes.length;
+  this.width = attributes.width;
+  this.height = attributes.height;
 }
 
 
@@ -158,7 +155,9 @@ function CuboidMaker(/*Your Code Here */){
   Create a method using CuboidMaker's prototype that returns the volume of a given cuboid's length, width, and height
   Formula for cuboid volume: length * width * height   */
 
-
+CuboidMaker.prototype.volume = function(){
+  return this.length*this.width*this.height
+}
 
 
 
@@ -167,14 +166,21 @@ function CuboidMaker(/*Your Code Here */){
   Formula for cuboid surface area of a cube: 
   2 * (length * width + length * height + width * height)  */
 
-
+CuboidMaker.prototype.surfaceArea = function(){
+  return 2*(this.length*this.width + this.length*this.height + this.width*this.height)
+}
 
 
 
 /* 🐴🐴🐴 Step 4: Create a new object that uses CuboidMaker 🐴🐴🐴
   Create a cuboid object that uses the new keyword to use our CuboidMaker constructor
   Add properties and values of length: 4, width: 5, and height: 5 to cuboid. */
-
+  
+const cuboid = new CuboidMaker({
+  length:4,
+  width:5,
+  height:5,
+})
 
 
 
@@ -188,8 +194,24 @@ function CuboidMaker(/*Your Code Here */){
 // 🦄🦄🦄 Topic 4: Classes 🦄🦄🦄 //
 //🦄🦄🦄 1. Take your prototypes from above and refactor into class syntax. Please rename your class CuboidMakerTwo and your object cuboidTwo 🦄🦄🦄
 class CuboidMakerTwo{
-
+  constructor(attributes){
+    this.length = attributes.length;
+    this.width = attributes.width;
+    this.height = attributes.height;
+  }
+  volume(){
+    return this.length*this.width*this.height
+  }
+  surfaceArea(){
+    return 2*(this.length*this.width + this.length*this.height + this.width*this.height)
 }
+}
+
+const cuboidTwo = new CuboidMakerTwo({
+  length:4,
+  width:5,
+  height:5,
+})
 
 
 //🦄🦄🦄 Test your volume and surfaceArea methods by uncommenting the logs below: 🦄🦄🦄
@@ -202,7 +224,17 @@ class CuboidMakerTwo{
 
 // 🦄 💪 Stretch Task: Extend the base class CuboidMaker with a sub class called CubeMaker.  Find out the formulas for volume and surface area for cubes and create those methods using the dimension properties from CuboidMaker.  Test your work by logging out your volume and surface area. 🦄 💪
   
-
+class CubeMaker extends CuboidMakerTwo{
+  constructor(attributes){
+    super(attributes);
+  }
+  cubeVolume(){
+    return this.length^3
+  }
+  cubeSurfaceArea(){
+    return (this.length^2)*6
+  }
+}
 
 
 
